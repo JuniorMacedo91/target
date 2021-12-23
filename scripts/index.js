ec02c49acf240d32496a5168f5ebc0c122e845b6
unknown <devfrontend01@gmail.com>
2021-12-23 01:23:21 -0300

    firebase config added to start firebase

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAW7q5YRuHFBR3QhCwIh8ljZGpqnzJQ-Ro",
+    authDomain: "target-nps.firebaseapp.com",
+    projectId: "target-nps",
+    storageBucket: "target-nps.appspot.com",
+    messagingSenderId: "954987526576",
+    appId: "1:954987526576:web:b8da8bfccfcec14cb63e75"
+  };
+
+  initializeApp(firebaseConfig);
