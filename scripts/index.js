7132ebcb206126fd7f1d020a4d094387e3532fa2
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-12-23 23:22:54 -0300

    Searching function created to find datas

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,36 @@ const firebaseConfig = {
     appId: "1:954987526576:web:b8da8bfccfcec14cb63e75"
   };
 
-  initializeApp(firebaseConfig);
+
+const employeeName= document.querySelector('#employee__name')
+const job = document.querySelector('#employee__job')
+const rn = document.querySelector('#rn')
+const detractors = document.querySelector('#detractors')
+const passives = document.querySelector('#passives')
+const totalNps = document.querySelector('#total-nps')
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore()
+
+let employeeID = '1341115';
+
+const employeeRef = db.collection('Supervisora Vanessa').doc(employeeID);
+
+employeeRef.get().then(doc =>{
+
+//   console.log(doc.data())
+})
+
+
+// Buscas
+
+const rankingEmployees = document.querySelector('.ranking-employees')
+
+db.collection('Supervisora Vanessa').where('nps', '>=', 75).get()
+            .then(snapshot =>{
+                snapshot.forEach(doc =>{
+                    let employee = doc.data();
+                    rankingEmployees.innerHTML += `<li>${employee.nome}</li>`
+                })
+            })
