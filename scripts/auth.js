98a4dba6f76a80caa403c5679195373f08414a82
unknown <devfrontend01@gmail.com>
2021-12-24 22:36:55 -0300

    sigIn function created

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,8 +1,37 @@
-import {db} from './index.js'
+const auth = firebase.auth()
 
-db.collection('Supervisora Vanessa').get()
-        .then(snapshot =>{
-            snapshot.forEach(doc =>{
-                console.log(doc.data())
+const email = document.querySelector('#email').value
+const password = document.querySelector('#password').value
+const loginBtn = document.querySelector('#login-btn')
+
+loginBtn.addEventListener('click', signIn)
+
+const newUserEmail = 'admintarget@target.com'
+const newPassword = 'target123'
+
+function createUser(){
+    auth.createUserWithEmailAndPassword(newUserEmail, newPassword)
+            .then(user =>{
+                alert(`Usuário cadastrado com sucesso`)
+            }).catch(error =>{
+                alert(`${error.message}`)
             })
-        })
+}
+
+function signIn(){
+    preventDefault();
+    auth.signInWithEmailAndPassword(newUserEmail, newPassword)
+            .then(() =>{
+                
+                if(newUserEmail === email){
+                    alert('Bem - vindo(a)')
+                    window.open('./dashboard.html')
+                }else{
+                    alert('Usuário não autorizado')
+                }
+                
+            }).catch( error =>{
+                alert('Usuário ou senha inválida')
+                throw error
+            })
+}
