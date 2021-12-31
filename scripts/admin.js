327d07b6ead75ea531deeefe24191ecb994be868
unknown <devfrontend01@gmail.com>
2021-12-31 16:29:25 -0300

    Autentication, login, logout functions created

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,7 +1,76 @@
+import {db} from './index.js'
+
+const employeeId = document.querySelector('#employeeId')
+const job = document.querySelector('#job')
+const fullName = document.querySelector('#employeeName')
+const email = document.querySelector('#email')
+const supervisor = document.querySelector('#supervisor')
+const place = document.querySelector('#place')
+const addNewEmployeeBtn = document.querySelector('.add-btn')
+const creatAccessBtn = document.querySelector('.creatAccess-btn')
+const signOutBtn = document.querySelector('#sign-out-btn')
+
+
+addNewEmployeeBtn.addEventListener('click', creatNewEmployee)
+creatAccessBtn.addEventListener('click', createUserLogIn)
+signOutBtn.addEventListener('click', logOut)
+
+
+function creatNewEmployee(){
+    db.collection('vanessa').doc(employeeId.value).set(
+        {
+            employeeId: employeeId.value,
+            job: job.value,
+            fullName: fullName.value,
+            email: email.value,
+            supervisor: supervisor.value,
+            place: place.value
+        }
+    ).then(() =>{
+        alert('Colaborador criado com sucesso')
+    }).catch(error =>{
+        console.log(error)
+    })
+}
+
+
+function createUserLogIn(){
+    event.preventDefault()
+    const newUserEmail = document.querySelector('#newEmployeeEmail').value
+    const newPassword =  document.querySelector('#newEmployeePassword').value
+
+    if(newUserEmail === '' || newPassword === ' '){
+        alert('Preencha todos os campos')
+    } else{
+        firebase.auth().createUserWithEmailAndPassword(newUserEmail, newPassword)
+            .then(() =>{
+                alert('Log in do Colaborador criado com sucesso')
+            }).catch(error =>{
+                console.log(`${error.message}`)
+            })
+    }
+}
+
+
+function logOut(){
+    firebase.auth().signOut()
+            .then(() =>{
+                if(confirm('Deseja sair?')){
+                    window.open('./index.html', '_self')
+                }
+                
+            }).catch(error =>{
+                console.log(error.message)
+            })
+}
+
+
 const tab = document.querySelector('#admin-page')
 const tabButton = document.querySelectorAll('.tab-button')
 const tabContent = document.querySelectorAll('.tab-content')
 
+
+// Change tab content
 tab.onclick = event =>{
     const id = event.target.dataset.id
 
@@ -19,3 +88,7 @@ tab.onclick = event =>{
         element.classList.add('active')
     }
 }
+
+
+
+
--- scripts/auth.js
+++ scripts/auth.js
@@ -1,39 +1,28 @@
-const auth = firebase.auth()
-
-const loginBtn = document.querySelector('#login-btn')
+const loginBtn = document.querySelector('.login-btn')
 
 loginBtn.addEventListener('click', signIn)
 
-function createUser(){
-    const newUserEmail = 'admintarget@target.com'
-    const newPassword = 'target123'
-
-    auth.createUserWithEmailAndPassword(newUserEmail, newPassword)
-            .then(user =>{
-                alert(`Usuário cadastrado com sucesso`)
-            }).catch(error =>{
-                alert(`${error.message}`)
-            })
-}
-
 function signIn(){
-    event.preventDefault();
+    event.preventDefault()
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
 
-    auth.signInWithEmailAndPassword(email, password)
-            .then(user =>{
-                
-                if(email === 'admintarget@target.com'){
-                    window.open('./admin.html')
-                }else{
-                    alert('Usuário não autorizado')
-                }
-                
-            }).catch( error =>{
-                alert('Usuário ou senha inválida')
-                throw error
-            })
+    if(email === ' ' || password === ''){
+        alert('Preencha todos os campos')
+    }  else{
+        firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(() =>{
+                    if(email === 'admintarget@target.com'){
+                        window.open('./admin.html', '_self')
+                    }else{
+                        window.open('./dashboard.html','_self')
+                    }
+                    
+                }).catch(error =>{
+                    alert('Usuário ou senha inválida')
+                })
+        }   
 }
+
 
 
